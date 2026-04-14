8ba39305f87826479cf7743b5cba732c2b8176b7
Amin <mansurov12133@gmail.com>
2026-04-14 20:27:04 +0800

    commit

--- main.cpp
+++ main.cpp
@@ -2,13 +2,23 @@
 #include<vector>
 
 class Card{
+    private:
+        std::vector<char>valueCardUI {'J', 'Q', 'K', 'A'};
     public:
         char suitCard;
         int valueCard;
         void PrintCard(){
             std::cout << suitCard << " ";
-            std::cout << valueCard<<'\n' ;
+            if(valueCard > 1)// to check whether J card and above or not
+                std::cout << valueCardUI[valueCard-2]<< '\n';
+            else
+                std::cout << valueCard+9<<'\n' ;
         }
+      Card(char suit, int value) {
+        suitCard = suit;
+        valueCard = value;
+    }
+
 };
 class Deck{
     public:
@@ -21,7 +31,7 @@ class Deck{
             {
                 for (int j = 0; j < 6; j++)
                 {   
-                    Card a {suits[i], j};
+                    Card a (suits[i], j);
                     deck.push_back(a);
                 }
                 
